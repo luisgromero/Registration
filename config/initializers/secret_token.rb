0ea66ffbb71bc3a7bb90b45849bf4099213dc21f
Luis G Romero <luisgromero@live.com>
2013-06-12 00:35:55 -0700

    Setup / root of app
    -Deleted index.html from public dir.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Registration::Application.config.secret_token = '6509dea60694a211e88284598276346a0da6ed64a50e59d3882f1a523b4ebcb46356fa3e813a72c8b1689f3ecf5a2f8af652dff8d06822733cfbbaf492b14536'
